340cc8bfbdc0ed834cc7a17aceb42141f5755716
Richard Neumann <mail@richard-neumann.de>
2018-09-13 21:25:13 +0200

    Implementing config parsing.

--- macreg/orm.py
+++ macreg/orm.py
@@ -1,18 +1,29 @@
 """Object relational mappings."""
 
+from ipaddress import IPv4Network
+
 from peewee import CharField, FixedCharField
 
-from peeweeplus import MySQLDatabase, JSONModel
+from peeweeplus import MySQLDatabase, JSONModel, IPv4AddressField
+
+
+CONFIG = ConfigParser()
+CONFIG.read('/etc/macreg.conf')
+NETWORK = IPv4Network(CONFIG['network']['network'])
+DATABASE = MySQLDatabase.from_config(CONFIG['db'])
 
 
 class NetworkExhausted(Exception):
     """Indicates that no more IPv4 addresses are available."""
-    
+
     pass
 
 
 class MacWhitelist(JSONModel):
     """A white list for MAC addresses."""
+
+    class Meta:
+        database = DATABASE
 
     user_name = CharField(255)
     first_name = CharField(255)
@@ -26,19 +37,19 @@ class MacWhitelist(JSONModel):
         """Yields the used IPv4 addresses."""
         for record in cls:
             yield record.ipv4address
-            
+
     @classmethod
     def free_ipv4address(cls, network):
         """Returns the lowest freee IPv4 address."""
         ipv4addresses = set(cls.ipv4addresses())
-        
+
         for ipv4address in network:
             if ipv4address not in ipv4addresses:
                 return ipv4address
-            
+
         raise NetworkExhausted()
-            
+
     def enable(self):
         """Enables the record."""
         if self.ipv4address is None:
-            self.ipv4address = type(self).free_ipv4address()  
+            self.ipv4address = type(self).free_ipv4address()
