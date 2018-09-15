1dffd56ea82308291f01c0f63319b4e91d6bfb80
Richard Neumann <mail@richard-neumann.de>
2018-09-15 14:34:11 +0200

    fixed packages.

--- backend/setup.py
+++ backend/setup.py
@@ -6,7 +6,7 @@ setup(
     name='macreg',
     author='Richard Neumann',
     author_email='<mail at richard dash neumann dot de>',
-    py_modules=['httpam'],
+    packages=['macreg'],
     url='https://github.com/coNQP/macreg',
     license='GPLv3',
     description='A MAC address registration web API.')
