e20369188939d4f986b48532723848006083380a
Richard Neumann <mail@richard-neumann.de>
2018-09-15 12:41:25 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='macreg',
+    author='Richard Neumann',
+    author_email='<mail at richard dash neumann dot de>',
+    py_modules=['httpam'],
+    url='https://github.com/coNQP/macreg',
+    license='GPLv3',
+    description='A MAC address registration web API.')
